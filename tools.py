12ca47ebf0d4955e3224aba14ffc9907baeb511c
fumiya-kubota <fumiya.kubota@glucose.jp>
2017-04-17 21:46:50 +0900

    hoge

--- tools.py
+++ tools.py
@@ -1,16 +1,22 @@
 import click
-from scipy import linalg, mat, dot
+from scipy import linalg, dot
 from datetime import datetime
 import numpy as np
-import time
 from multiprocessing import Pool, cpu_count
 import os
 import shutil
 import glob
+from itertools import chain
 
 
 def calc_similarity(v1, v2):
     return dot(v1, v2) / linalg.norm(v1) / linalg.norm(v2)
+
+
+def similarity_tokens(args):
+    similarity_func, vec, vec_dictionary, border = args
+    values = ((t, similarity_func(vec, v)) for t, v in vec_dictionary.items())
+    return list(filter(lambda ts: ts[1] >= border, values))
 
 
 @click.group()
@@ -18,23 +24,21 @@ def cmd():
     pass
 
 
-def make_preparation_dir(vec_file, vocab_file, dir_name):
+def make_preparation_dir(vec_file, dir_name):
     if os.path.exists(dir_name):
         shutil.rmtree(dir_name)
     os.makedirs(dir_name)
 
     vec_fps = [open(os.path.join(dir_name, '{}.vec'.format(idx)), 'w') for idx in range(1, cpu_count() + 1)]
-    for idx, row in enumerate(open(vec_file)):
-        fp = vec_fps[idx % len(vec_fps)]
-        fp.write('{}'.format(row))
+    with open(vec_file) as fp:
+        fp.readline()
+        for idx, row in enumerate(fp):
+            fp = vec_fps[idx % len(vec_fps)]
+            fp.write('{}'.format(row))
 
 
 def vec_dictionary_from_file_path(file_path):
-    print(file_path, 'start')
-    a = {row[0]: np.array(row[1:], dtype=float) for row in (row.strip().split() for row in open(file_path))}
-    print(file_path, 'end')
-    return a
-
+    return {row[0]: np.array(row[1:], dtype=float) for row in (row.strip().split() for row in open(file_path))}
 
 
 class VecDirDict:
@@ -52,36 +56,43 @@ class VecDirDict:
                 return dictionary[key]
         return None
 
-
-def near_similarity_tokens(file_path, vec, cmp_func, border):
-    for token, matrix in iter_vec_value(file_path):
-        if vec.shape == matrix.shape:
-            similarity = cmp_func(vec, matrix)
-            if similarity >= border:
-                yield token, similarity
+    def similarity_tokens(self, vec, border):
+        return chain.from_iterable(Pool().map(
+            similarity_tokens,
+            ((calc_similarity, vec, d, border) for d in self.dicts)
+        ))
 
 
 @cmd.command()
 @click.argument('vec_path', type=click.STRING)
-@click.argument('dir_name', type=click.STRING)
-def prepare(vec_path, vocab_path, dir_name):
+@click.argument('dir_name', type=click.STRING, default='preparation_dir')
+def prepare(vec_path, dir_name):
     if os.path.exists(dir_name):
         shutil.rmtree(dir_name)
-    make_preparation_dir(vec_path, vocab_path, dir_name)
+    make_preparation_dir(vec_path, dir_name)
 
 
 @cmd.command()
 @click.argument('vocab', type=click.STRING)
 @click.argument('vocab_path', type=click.STRING)
-@click.argument('preparation_dir', type=click.STRING)
-def distance(vocab, vocab_path, preparation_dir):
+@click.argument('preparation_dir', type=click.STRING, default='preparation_dir')
+@click.argument('border', type=click.FLOAT, default=0.65)
+def distance(vocab, vocab_path, preparation_dir, border):
     assert os.path.exists(preparation_dir)
     token = vocab2token(vocab, vocab_path)
     click.echo(token)
     now = datetime.now()
     vec_dict = VecDirDict(preparation_dir)
-    print(vec_dict.get(token))
     print(datetime.now() - now)
+
+    now = datetime.now()
+    values = vec_dict.similarity_tokens(vec_dict.get(token), border)
+    print(datetime.now() - now)
+    values = sorted(values, key=lambda ts: ts[1], reverse=True)
+    print(values)
+    t2v = tokens2vocab([t for t, s in values], vocab_path)
+    for t, s in values:
+        click.echo('{} {}'.format(t2v.get(t), s))
 
 
 def vocab2token(vocab, path):
@@ -93,15 +104,26 @@ def vocab2token(vocab, path):
     return None
 
 
-# @cmd.command()
-# @click.argument('token', type=click.INT)
-# @click.argument('path', type=click.Path(exists=True, file_okay=True, dir_okay=False))
-# def token2vocab(token, path):
-#     with open(path) as fp:
-#         for idx, row in enumerate(map(lambda x: x.strip(), fp)):
-#             if token == idx:
-#                 return row
-#     return None
+def tokens2vocab(tokens, path):
+    result = {}
+    print(tokens, path)
+    with open(path) as fp:
+        for idx, row in enumerate(map(lambda x: x.strip(), fp)):
+            idx = str(idx)
+            if idx in tokens:
+                result[idx] = row
+                tokens.remove(idx)
+                if not tokens:
+                    break
+    return result
+
+
+@cmd.command()
+@click.argument('token', type=click.INT)
+@click.argument('path', type=click.Path(exists=True, file_okay=True, dir_okay=False))
+def token2vocab(token, path):
+    print(token, path)
+    print(tokens2vocab([token], path))
 
 
 if __name__ == '__main__':
